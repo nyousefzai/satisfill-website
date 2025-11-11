080527e045c9ce7eb71460731951224bf2955a97
Navid Yousefzai <navid22y@gmail.com>
2025-11-10 16:31:07 -0800

    Fix: Properly handle QueryClient initialization for SSR

--- src/lib/providers.tsx
+++ src/lib/providers.tsx
@@ -1,5 +1,9 @@
 "use client";
 
+import { Toaster } from "@/components/ui/sonner";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { type ReactNode, useState } from "react";
+
 function makeQueryClient() {
   return new QueryClient({
     defaultOptions: {
@@ -19,16 +23,22 @@ function makeQueryClient() {
 let browserQueryClient: QueryClient | undefined = undefined;
 
 function getQueryClient() {
-  if (!browserQueryClient) browserQueryClient = makeQueryClient();
-  return browserQueryClient;
+  if (typeof window === "undefined") {
+    // Server: always make a new query client
+    return makeQueryClient();
+  } else {
+    // Browser: make a new query client if we don't already have one
+    if (!browserQueryClient) browserQueryClient = makeQueryClient();
+    return browserQueryClient;
+  }
 }
 
-import { Toaster } from "@/components/ui/sonner";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { type ReactNode } from "react";
-
 export default function Providers({ children }: { children: ReactNode }) {
-  const queryClient = getQueryClient();
+  // NOTE: Avoid useState when initializing the query client if you don't
+  //       have a suspense boundary between this and the code that may
+  //       suspend because React will throw away the client on the initial
+  //       render if it suspends and there is no boundary
+  const [queryClient] = useState(() => getQueryClient());
 
   return (
     <QueryClientProvider client={queryClient}>
